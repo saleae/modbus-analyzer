f77f467df25b850225bd4dd2277ff972a67d10f9
Marcus10110 <markgarrison@saleae.com>
2020-10-29 15:06:39 -0700

    clang-format update of Modbus Analyzer

--- src/ModbusAnalyzer.h
+++ src/ModbusAnalyzer.h
@@ -12,54 +12,54 @@
 class ModbusAnalyzerSettings;
 class ModbusAnalyzer : public Analyzer2
 {
-public:
-	ModbusAnalyzer();
-	virtual ~ModbusAnalyzer();
-	virtual void SetupResults();
-	virtual void WorkerThread();
+  public:
+    ModbusAnalyzer();
+    virtual ~ModbusAnalyzer();
+    virtual void SetupResults();
+    virtual void WorkerThread();
 
-	virtual U32 GenerateSimulationData( U64 newest_sample_requested, U32 sample_rate, SimulationChannelDescriptor** simulation_channels );
-	virtual U32 GetMinimumSampleRateHz();
+    virtual U32 GenerateSimulationData( U64 newest_sample_requested, U32 sample_rate, SimulationChannelDescriptor** simulation_channels );
+    virtual U32 GetMinimumSampleRateHz();
 
-	virtual const char* GetAnalyzerName() const;
-	virtual bool NeedsRerun();
-	
+    virtual const char* GetAnalyzerName() const;
+    virtual bool NeedsRerun();
 
 
 #pragma warning( push )
-#pragma warning( disable : 4251 ) //warning C4251: 'ModbusAnalyzer::<...>' : class <...> needs to have dll-interface to be used by clients of class
+#pragma warning(                                                                                                                           \
+    disable : 4251 ) // warning C4251: 'ModbusAnalyzer::<...>' : class <...> needs to have dll-interface to be used by clients of class
 
-protected: //functions
-	void ComputeSampleOffsets();
-	U64 GetNextByteModbus(U32 num_bits, U64 bit_mask, U64 &frame_starting_sample, U64 &frame_ending_sample);
-	int ASCII2INT(char value);
+  protected: // functions
+    void ComputeSampleOffsets();
+    U64 GetNextByteModbus( U32 num_bits, U64 bit_mask, U64& frame_starting_sample, U64& frame_ending_sample );
+    int ASCII2INT( char value );
 
-protected: //vars
-	std::auto_ptr< ModbusAnalyzerSettings > mSettings;
-	std::auto_ptr< ModbusAnalyzerResults > mResults;
-	AnalyzerChannelData* mModbus;
+  protected: // vars
+    std::auto_ptr<ModbusAnalyzerSettings> mSettings;
+    std::auto_ptr<ModbusAnalyzerResults> mResults;
+    AnalyzerChannelData* mModbus;
 
-	ModbusSimulationDataGenerator mSimulationDataGenerator;
-	bool mSimulationInitilized;
+    ModbusSimulationDataGenerator mSimulationDataGenerator;
+    bool mSimulationInitilized;
 
-	//Modbus analysis vars:
-	U32 mSampleRateHz;
-	std::vector<U32> mSampleOffsets;
-	U32 mParityBitOffset;
-	U32 mStartOfStopBitOffset;
-	BitState mBitLow;
-	BitState mBitHigh;
+    // Modbus analysis vars:
+    U32 mSampleRateHz;
+    std::vector<U32> mSampleOffsets;
+    U32 mParityBitOffset;
+    U32 mStartOfStopBitOffset;
+    BitState mBitLow;
+    BitState mBitHigh;
 
-	//Checksum caluclations for Modbus
-	U16   crc_tab16[256];
-	void init_crc16_tab( void );
-	U16 update_CRC( U16 crc, U8 c );
+    // Checksum caluclations for Modbus
+    U16 crc_tab16[ 256 ];
+    void init_crc16_tab( void );
+    U16 update_CRC( U16 crc, U8 c );
 
 #pragma warning( pop )
 };
 
 extern "C" ANALYZER_EXPORT const char* __cdecl GetAnalyzerName();
-extern "C" ANALYZER_EXPORT Analyzer* __cdecl CreateAnalyzer( );
+extern "C" ANALYZER_EXPORT Analyzer* __cdecl CreateAnalyzer();
 extern "C" ANALYZER_EXPORT void __cdecl DestroyAnalyzer( Analyzer* analyzer );
 
-#endif //MODBUS_ANALYZER_H
+#endif // MODBUS_ANALYZER_H
--- src/ModbusAnalyzerResults.h
+++ src/ModbusAnalyzerResults.h
@@ -15,22 +15,21 @@ class ModbusAnalyzerSettings;
 
 class ModbusAnalyzerResults : public AnalyzerResults
 {
-public:
-	ModbusAnalyzerResults( ModbusAnalyzer* analyzer, ModbusAnalyzerSettings* settings );
-	virtual ~ModbusAnalyzerResults();
+  public:
+    ModbusAnalyzerResults( ModbusAnalyzer* analyzer, ModbusAnalyzerSettings* settings );
+    virtual ~ModbusAnalyzerResults();
 
-	virtual void GenerateBubbleText( U64 frame_index, Channel& channel, DisplayBase display_base );
-	virtual void GenerateExportFile( const char* file, DisplayBase display_base, U32 export_type_user_id );
+    virtual void GenerateBubbleText( U64 frame_index, Channel& channel, DisplayBase display_base );
+    virtual void GenerateExportFile( const char* file, DisplayBase display_base, U32 export_type_user_id );
 
-	virtual void GenerateFrameTabularText(U64 frame_index, DisplayBase display_base );
-	virtual void GeneratePacketTabularText( U64 packet_id, DisplayBase display_base );
-	virtual void GenerateTransactionTabularText( U64 transaction_id, DisplayBase display_base );
+    virtual void GenerateFrameTabularText( U64 frame_index, DisplayBase display_base );
+    virtual void GeneratePacketTabularText( U64 packet_id, DisplayBase display_base );
+    virtual void GenerateTransactionTabularText( U64 transaction_id, DisplayBase display_base );
 
-protected: //functions
-
-protected:  //vars
-	ModbusAnalyzerSettings* mSettings;
-	ModbusAnalyzer* mAnalyzer;
+  protected: // functions
+  protected: // vars
+    ModbusAnalyzerSettings* mSettings;
+    ModbusAnalyzer* mAnalyzer;
 };
 
-#endif //MODBUS_ANALYZER_RESULTS
+#endif // MODBUS_ANALYZER_RESULTS
